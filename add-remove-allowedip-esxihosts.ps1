975462f33f9b473026e74d504d5ad5d4b7437ae1
Jason Shiplett <jason@shiplett.org>
2026-07-27 15:34:41 -0400

    Added documentation for add-remove-allowedip-esxihosts.ps1

--- add-remove-allowedip-esxihosts.ps1
+++ add-remove-allowedip-esxihosts.ps1
@@ -1,3 +1,23 @@
+<#
+    .SYNOPSIS
+    Add or remove a list of ESXi host firewall rulesets based on CSV input
+
+    .DESCRIPTION
+    This script adds or removes a list of ESXi host firewall rulesets based on CSV input
+
+    .EXAMPLE
+    .\add-remove-allowedip-esxihosts.ps1 -Action Add -csvInput ..\esxi-host-allowedip.csv
+
+    .EXAMPLE
+    .\add-remove-allowedip-esxihosts.ps1 -Action Remove -csvInput ..\esxi-host-allowedip.csv
+
+    .PARAMETER Action
+    The action to be taken, either Add or Remove
+
+    .PARAMETER csvInput
+    CSV input for script execution
+#>
+
 param(
     [Parameter(Mandatory)][ValidateSet("Add","Remove")][string]$AddorRemove,
     [Parameter(Mandatory)][ValidateNotNullOrEmpty()][string]$csvInput
